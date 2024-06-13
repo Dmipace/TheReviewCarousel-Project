8708659f2957f56a22f91d9596e8eb1d3364e660
Dmipace <tomas11sivak@gmail.com>
2024-06-13 14:08:24 +0200

    Odoslanie projektu

---
+++ script.js
@@ -0,0 +1,100 @@
+//1
+const reviews = [
+    {
+      id: 1,
+      name: 'susan smith',
+      job: 'web developer',
+      img: 'https://www.course-api.com/images/people/person-1.jpeg',
+      text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: 'anna johnson',
+      job: 'web designer',
+      img: 'https://www.course-api.com/images/people/person-2.jpeg',
+      text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    },
+    {
+      id: 3,
+      name: 'peter jones',
+      job: 'intern',
+      img: 'https://www.course-api.com/images/people/person-4.jpeg',
+      text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    },
+    {
+      id: 4,
+      name: 'bill anderson',
+      job: 'the boss',
+      img: 'https://www.course-api.com/images/people/person-3.jpeg',
+      text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    },
+  ];
+  
+//2  
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+//3
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+  
+//4
+// set starting item
+let currentItem = 0;
+
+//5
+//do img,author,job dávam hodnoty z reviews
+window.addEventListener("DOMContentLoaded", function(){
+    showPerson(currentItem);
+})
+  
+//6
+//show person based on item // zobrazí osobu a komplet info o nej
+function showPerson(person){    //môže byť aj prazdne showperson všade a potom tu hned do reviews daj currentItem v []
+    const item = reviews[person]
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+
+//show next person
+nextBtn.addEventListener("click", function(){
+    currentItem++;
+    if(currentItem > reviews.length - 1){
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+})
+
+//show prev person
+prevBtn.addEventListener("click", function(){
+    currentItem--;
+    if(currentItem < 0){
+        currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+})
+
+
+//zobrazí random osobu a všetky info o nej cez tlačidlo random button
+randomBtn.addEventListener("click", function(){
+    currentItem = Math.floor(Math.random() * reviews.length);
+    console.log(currentItem);
+    showPerson(currentItem);
+})
+
+
+
+
+
+
+
+
+
+
+
+
